5a8e5feddfc14282a22ae2951ef65c55d90d2281
Karl Krauth <krauth@krauth-macbookair.roam.corp.google.com>
2015-03-17 23:05:40 +1100

    small style improvements on implementation

--- components.py
+++ components.py
@@ -1,14 +1,13 @@
-'''This module contains a data structure to efficiently keep track of 
-what components are connected to each other and what component-wide features
-are active.'''
+"""Efficiently keep track of what components are connected to each other."""
 
 import sets
 import sortedcontainers
 
+
 class Components(object):
   class Node(object):
-    def __init__(self, id):
-      self.id = id
+    def __init__(self, node_id):
+      self.id = node_id
       self.parent = None
       self.size = 1
       self.depth = 0
@@ -19,7 +18,7 @@ class Components(object):
     self._component_sizes = sortedcontainers.SortedList()
 
   def connect(self, id1, id2):
-    '''Connect two components together.'''
+    """Connect two components together."""
     node1 = self._get_top(id1)
     node2 = self._get_top(id2)
 
@@ -36,26 +35,27 @@ class Components(object):
       self._component_sizes.add((node1.size, node1.id))
 
   def add_feature(self, node_id, feature_id):
-    '''Add a feature to the component the node_id belongs to.'''
+    """Add a feature to the component the node_id belongs to."""
     node = self._get_top(node_id)
     node.features.add(feature_id)
 
   def has_feature(self, node_id, feature_id):
-    '''Check if the component the node belongs to has the given feature.'''
+    """Check if the component the node belongs to has the given feature."""
     node = self._get_top(node_id)
     return feature_id in node.features
 
-  def component_size(self, id):
-    '''Get the size of the component the node belongs to'''
-    node = self._get_top(id)
+  def component_size(self, node_id):
+    """Get the size of the component the node belongs to."""
+    node = self._get_top(node_id)
     return node.size
 
   def split_component(self, ids1, ids2):
-    '''Split a component into two parts.
+    """Split a component into two parts.
 
-    ids1 and ids2 are disjoint list of ids that represent the two new
-    desired components.
-    '''
+    Args:
+      ids1 (list): The set of ids in the first new component.
+      ids2 (list): The set of ids in the second new component.
+    """
     if len(ids1) == 0 or len(ids2) == 0:
       return
 
@@ -84,41 +84,40 @@ class Components(object):
     self._component_sizes.add((parent_node1.size, parent_node1.id))
     self._component_sizes.add((parent_node2.size, parent_node2.id))
 
-  def add_id(self, id):
-    '''Add a single node component with the given id.'''
-    if self._id_dict.get(id) is not None:
+  def add_id(self, node_id):
+    """Add a single node component with the given id."""
+    if self._id_dict.get(node_id) is not None:
       return
 
-    node = self.Node(id)
-    self._id_dict[id] = node
+    node = self.Node(node_id)
+    self._id_dict[node_id] = node
     self._component_sizes.add((node.size, node.id))
 
   def get_sorted_sizes(self):
-    '''Gets a list sorted by size of all components.
-    
-    The return list consists of tuples where the first element is the size
-    of the component and the second element is an id of a node belonging to
-    the component.
-    '''
-  
+    """Gets a list sorted by size of all components.
+
+    Returns:
+      list: A list of tuples containing the size of the component and
+      a node in the component.
+    """
+
     return self._component_sizes.as_list()
 
   def _merge_nodes(self, parent, child):
-    '''Merge two components together and combine their features'''
+    """Merge two components together and combine their features."""
     child.parent = parent
     parent.size += child.size
     if parent.depth == child.depth:
-      parent.depth += 1 
+      parent.depth += 1
     parent.features = parent.features.union(child.features)
     child.features.clear()
-    
 
-  def _get_top(self, id):
-    '''Get the top level node of a component'''
-    curr_node = self._id_dict.get(id)
+  def _get_top(self, node_id):
+    """Get the top level node of a component."""
+    curr_node = self._id_dict.get(node_id)
     assert curr_node is not None
 
     while curr_node.parent is not None:
       curr_node = curr_node.parent
-    
+
     return curr_node
--- infection_graph.py
+++ infection_graph.py
@@ -1,9 +1,14 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-import sets
-import components
-import Queue
+"""Provides an implementation of an infection graph for A/B testing."""
+
 import array
+import Queue
+import sets
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+import components
+
 
 class InfectionGraph(object):
   def __init__(self):
@@ -11,51 +16,43 @@ class InfectionGraph(object):
     self._components = components.Components()
 
   def add_user(self, user_id):
-    '''Add a new user to the graph'''
+    """Add a new user to the graph."""
     self._graph.add_node(user_id)
     self._components.add_id(user_id)
 
   def remove_user(self, user_id):
-    '''Remove a user from the graph'''
+    """Remove a user from the graph."""
     self._graph.remove_node(user_id)
     assert False, "Method not implemented"
 
   def add_coaching_relation(self, coach_id, student_id):
-    '''Set a given user as the coach of another user'''
+    """Set a given user as the coach of another user."""
     self._graph.add_edge(coach_id, student_id)
     self._components.connect(coach_id, student_id)
 
   def remove_coaching_relation(self, coach_id, student_id):
-    '''Remove the given user from the list of the coach's students.'''
+    """Remove the given user from the list of the coach's students."""
     self._graph.remove_edge(coach_id, student_id)
     nodes1 = self._get_connected(coach_id, student_id)
-    if nodes1 == None:
+    if nodes1 is None:
       return
     nodes2 = self._get_connected(student_id, coach_id)
     self._components.split_component(list(nodes1), list(nodes2))
 
   def get_students(self, coach_id):
-    '''Get all the students of a given coach'''
+    """Get all the students of a given coach."""
     return self._graph.successors(coach_id)
 
   def get_coaches(self, student_id):
-    '''Get all the coaches of a given student'''
+    """Get all the coaches of a given student."""
     return self._graph.predecessors(student_id)
 
   def total_infection(self, user_id, feature_id):
-    '''Give all the users that are connected to the given user the given feature.
-
-    Note that the transferal of features is transitive and operates in both directions.
-    A student can infect their coach and vice versa.
-    '''
+    """Give the users in the component of the given user the given feature."""
     self._components.add_feature(user_id, feature_id)
 
   def approx_limited_infection(self, feature_id, approx_infections):
-    '''Give around approx_infections users the given feature.
-
-    Note that this function assumes that there will be a decent amount 
-    of single nodes or small components to work optimally.
-    '''
+    """Give around approx_infections users the given feature."""
     sizes = self._components.get_sorted_sizes()
     num_infected = 0
     infected = []
@@ -77,11 +74,14 @@ class InfectionGraph(object):
     return num_infected
 
   def exact_limited_infection(self, feature_id, num_infections):
-    '''Try to infect exactly num_infections users with the given feature.
+    """Try to infect exactly num_infections users with the given feature.
 
-    If we can't infect the given number return false. Otherwise return true and
-    infect them.
-    '''
+    Args:
+      feature_id (int): The id of the feature to infect users with.
+      num_infections (int): The number of users we want to infect.
+    Returns:
+      bool: True if we can infect exactly the number of users, False otherwise.
+    """
 
     # We can always infect 0 users.
     if num_infections == 0:
@@ -92,7 +92,8 @@ class InfectionGraph(object):
       return False
 
     # Create a len(sizes) x num_infections memoization array.
-    dp = [array.array('i', (-1,) * (num_infections + 1)) for i in xrange(len(sizes))]
+    dp = [array.array("i", (-1,) * (num_infections + 1))
+          for _ in xrange(len(sizes))]
 
     if not self._rec_exact_infection(sizes, len(sizes) - 1, num_infections, dp):
       return False
@@ -112,28 +113,35 @@ class InfectionGraph(object):
         infected.append(sizes[subset_end][1])
         num_to_infect -= sizes[subset_end][0]
         subset_end -= 1
-   
+
     # infect all the appropriate components.
     for user_id in infected:
       self.total_infection(user_id, feature_id)
-    return True  
+    return True
 
   def has_feature(self, user_id, feature_id):
     return self._components.has_feature(user_id, feature_id)
 
   def draw(self, dest):
-    plt.figure(1,figsize=(24,24))
+    plt.figure(1, figsize=(24, 24))
     # layout graphs with positions using graphviz neato
-    pos=nx.graphviz_layout(self._graph,prog="neato")
+    pos = nx.graphviz_layout(self._graph, prog="neato")
     nx.draw(self._graph, pos, node_size=5, with_labels=False)
     plt.savefig(dest, dpi=75)
 
   def _rec_exact_infection(self, sizes, subset_end, num_infect, dp):
-    '''Check if it's possible to infect num_infect users given the component sizes.
-   
-    subset_end determines the largest component we can use in our sizes array
-    when infecting users.
-    '''
+    """Check if it's possible to infect num_infect users.
+
+    Args:
+      sizes (list): Components with their associated size.
+      subset_end (int): The last element of sizes we can use.
+      num_infect (int): The number of users we want to infect.
+      dp (list): A list of arrays dp[i][j] will be set to True if we can infect
+      j users with elements of sizes up until index i. Initially dp[i][j]
+      is set to -1.
+    Returns:
+      int: 1 if we can infect the number of users given the limits 0 otherwise.
+    """
     if dp[subset_end][num_infect] != -1:
       return dp[subset_end][num_infect]
 
@@ -143,28 +151,32 @@ class InfectionGraph(object):
       else:
         dp[subset_end][num_infect] = 0
     else:
-      dp[subset_end][num_infect] = \
-          self._rec_exact_infection(sizes, subset_end - 1, num_infect, dp) or \
-          self._rec_exact_infection(sizes, subset_end - 1, num_infect - sizes[subset_end][0], dp) or \
-          (sizes[subset_end][0] == num_infect)
+      # Either the last element is of the exact size or we consider smaller
+      # elements.
+      dp[subset_end][num_infect] = (
+        self._rec_exact_infection(sizes, subset_end - 1, num_infect, dp) or
+        self._rec_exact_infection(sizes, subset_end - 1, num_infect -
+                                  sizes[subset_end][0], dp) or
+        (sizes[subset_end][0] == num_infect))
+
     return dp[subset_end][num_infect]
 
   def _get_connected(self, source, connected_node):
     visited = sets.Set()
     to_visit = Queue.Queue()
-    to_visit.put(source)   
-    visited.add(source)   
- 
+    to_visit.put(source)
+    visited.add(source)
+
     while not to_visit.empty():
       curr_id = to_visit.get()
       neighbors = self._get_neighbors(curr_id)
       for neighbor in neighbors:
-        if not neighbor in visited:
+        if neighbor not in visited:
           if neighbor == connected_node:
             return None
           to_visit.put(neighbor)
           visited.add(neighbor)
-    
+
     return visited
 
   def _get_neighbors(self, user_id):
